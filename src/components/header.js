8873badf37ed78a36be4fac4e209f146a5a11b9e
Shashank Shekhar <shashank7200@gmail.com>
2020-07-29 23:22:38 +0530

    added fonts and images

--- src/components/header.js
+++ src/components/header.js
@@ -1,6 +1,7 @@
 import { Link } from 'gatsby'
 import PropTypes from 'prop-types'
 import React from 'react'
+import Image from './image';
 
 const Header = ({ siteTitle }) => (
   <header>
@@ -9,6 +10,12 @@ const Header = ({ siteTitle }) => (
       <div className="rotate-wrapper vertical"><span className="rotate-inner rotate">Days</span></div>
       <div className="horizontal">100</div>
       <div className="rotate-wrapper vertical"><span className="rotate-inner rotate">Films</span></div>
+    </div>
+    <div className="header-links">
+      <div>
+        About
+      </div>
+      <Image src="color-picker.svg"/>
     </div>
   </header>
 )
