d22ef5e5ae62b296542950f4145be640274063c2
Shashank Shekhar <shashank7200@gmail.com>
2020-08-04 02:06:41 +0530

    updated styling and added film description element

--- src/components/FilmItem.js
+++ src/components/FilmItem.js
@@ -1,10 +1,11 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import PropTypes from 'prop-types';
 import StarRatings from 'react-star-ratings';
 import { ThemeContext } from '../store';
 import './FilmItem.css';
 
-export default function FilmItem({ imageUrl, filmName, filmRating, filmYear, filmDay, filmWatchDate }) {
+export default function FilmItem({ imageUrl, filmName, filmRating, filmYear, filmDay, filmWatchDate, filmDescription }) {
+	const [showDescription, setShowDescription ] = useState(false);
 	const { state } = useContext(ThemeContext);
 	return (
 		<div
@@ -14,71 +15,109 @@ export default function FilmItem({ imageUrl, filmName, filmRating, filmYear, fil
 				flexDirection: 'column',
 				alignContent: 'center',
 				width: 240,
-				marginBottom: 50,
 				// marginRight: 40,
 				boxShadow: '8px 8px 20px 8px rgba(0, 0, 0, 0.25)',
 				borderRadius: '18px',
-				fontFamily: "'Poppins', sans-serif"
+				fontFamily: "'Poppins', sans-serif",
+				margin: "0.6%",
+				marginBottom: 50,
 			}}
 		>
-			<div
-				style={{
-					position: 'absolute',
-					// background: state.backgroundColor,
-					// color: state.color,
-					backgroundColor: '#FED700',
-					color: '#222',
-					borderTopLeftRadius: 18,
-					fontSize: 14,
-					height: 'auto',
-					lineHeight: 'normal',
-					padding: '4px 6px',
-					zIndex: 10,
-					top: 0,
-					left: 0,
-				}}
-			>
-				<span>Day: { filmDay }</span>
-			</div>
-			<div
-				style={{
-					position: 'absolute',
-					// background: state.backgroundColor,
-					// color: state.color,
-					backgroundColor: '#e5a937',
-					color:'#fff',
-					borderTopRightRadius: 18,
-					fontSize: 14,
-					height: 'auto',
-					lineHeight: 'normal',
-					padding: '4px 6px',
-					zIndex: 10,
-					top: 0,
-					right: 0,
-				}}
-			>
-				<span>{ filmWatchDate }</span>
-			</div>
+			{ !showDescription &&
+				<div
+					style={{
+						position: 'absolute',
+						// background: state.backgroundColor,
+						// color: state.color,
+						backgroundColor: '#FED700',
+						color: '#222',
+						borderTopLeftRadius: 18,
+						fontSize: 14,
+						height: 'auto',
+						lineHeight: 'normal',
+						padding: '4px 6px',
+						zIndex: 10,
+						top: 0,
+						left: 0,
+					}}
+				>
+					<span>Day: { filmDay }</span>
+				</div>
+			}
+			{ !showDescription &&
+				<div
+					style={{
+						position: 'absolute',
+						// background: state.backgroundColor,
+						// color: state.color,
+						backgroundColor: '#e5a937',
+						color:'#fff',
+						borderTopRightRadius: 18,
+						fontSize: 14,
+						height: 'auto',
+						lineHeight: 'normal',
+						padding: '4px 6px',
+						zIndex: 10,
+						top: 0,
+						right: 0,
+					}}
+				>
+					<span>{ filmWatchDate }</span>
+				</div>
+			}
 			<div 
 				className="film-item-image"
 				style={{
 					height: 334,
+					backgroundColor: showDescription ? state.backgroundColor : 'inherit',
+					borderRadius: '18px 18px 0px 0px'
 				}}
+				onMouseEnter={() => setShowDescription(true)}
+				onMouseLeave={() => setShowDescription(false)}
 			>
-				<img 
-					src={imageUrl} alt={filmName}
-					style={{
-						width: '100%',
-						height: '100%',
-						filter: 'drop-shadow(0px 0px 4px rgba(0, 0, 0, 0.25))',
-						borderRadius: '18px 18px 0px 0px'
-					}}
-				/>
+				{
+					showDescription ?
+					(
+						<div
+							style={{
+								fontFamily: "'Poppins',inherit, sans-serif",
+								fontStyle: 'normal',
+								fontWeight: '600',
+								fontSize: 14,
+								padding: '76px 13px 42px 20px',
+								color: showDescription ? state.color : 'inherit',
+								display: 'flex',
+								alignItems: 'baseline',
+								fontFeatureSettings: "'pnum' on, 'lnum' on",
+								border: '3px solid',
+								borderColor: state.color,
+								height: '100%',
+								boxSizing: 'border-box',
+								borderRadius: '18px 18px 0px 0px'
+							}}
+						>
+							{ filmDescription }
+						</div>
+					) :
+					(
+						<img 
+							src={imageUrl} alt={filmName}
+							style={{
+								width: '100%',
+								height: '100%',
+								filter: 'drop-shadow(0px 0px 4px rgba(0, 0, 0, 0.25))',
+								borderRadius: '18px 18px 0px 0px',
+								objectFit: 'cover',
+								cursor: 'pointer'
+							}}
+						/>
+					)
+				}
 			</div>
 			<div
 				style={{
 					width: 'auto',
-					height: 66,
+					height: 84,
 					color: '#000',
 					background: '#FFFFFF',
 					borderRadius: '0px 0px 18px 18px',
@@ -86,7 +125,8 @@ export default function FilmItem({ imageUrl, filmName, filmRating, filmYear, fil
 					justifyContent: 'center',
 					flexDirection: 'column',
 					alignItems: 'center',
-					padding: '14px 8px'
+					padding: '14px 8px',
+					textAlign: 'center'
 				}}
 			>
 				<p>{ filmName }</p>
@@ -109,6 +149,7 @@ FilmItem.propTypes = {
 	filmName: PropTypes.string.isRequired,
 	filmYear: PropTypes.number.isRequired,
 	filmRating: PropTypes.number.isRequired,
-	filmDay: PropTypes.number.isRequired,
-	filmWatchDate: PropTypes.string
+	filmDay: PropTypes.string.isRequired,
+	filmWatchDate: PropTypes.string,
+	filmDescription: PropTypes.string.isRequired
 }
