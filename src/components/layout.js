1fc89037819f70550907a9218b46a1030b3fe9ca
Shashank Shekhar <shashank7200@gmail.com>
2020-08-04 22:19:53 +0530

    updated the layourt with selection colors and styled components

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,31 +1,51 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import PropTypes from 'prop-types'
 import { StaticQuery, graphql } from 'gatsby'
+import styled from 'styled-components';
 
 import Header from './header'
+import { ThemeContext } from './../store';
 import './layout.css'
 
-const Layout = ({ children }) => (
-  <StaticQuery
-    query={graphql`
-      query SiteTitleQuery {
-        site {
-          siteMetadata {
-            title
-          }
-        }
-      }
-    `}
-    render={data => (
-      <div className="app-wrapper">
-        <Header siteTitle={data.site.siteMetadata.title} />
-        <div>
-          {children}
-        </div>
-      </div>
-    )}
-  />
-)
+const Layout = ({ children }) => {
+
+	const { state } = useContext(ThemeContext);
+
+	const AppWrapper = styled.div`
+		max-width: 80%;
+		margin: 0 auto;
+
+		div,
+		p,
+		span {
+			&::selection {
+				background: ${state.color};
+				color: ${state.backgroundColor};
+			}
+		}
+	`
+	return (
+		<StaticQuery
+			query={graphql`
+			query SiteTitleQuery {
+				site {
+				siteMetadata {
+					title
+				}
+				}
+			}
+			`}
+			render={data => (
+			<AppWrapper>
+				<Header siteTitle={data.site.siteMetadata.title} />
+				<div>
+				{children}
+				</div>
+			</AppWrapper>
+			)}
+		/>
+	)
+}
 
 Layout.propTypes = {
   children: PropTypes.node.isRequired,
