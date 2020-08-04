04e6a37ed134c5420032d7464b6ce9192a22f983
Shashank Shekhar <shashank7200@gmail.com>
2020-08-04 19:10:58 +0530

    added styled components

--- gatsby-config.js
+++ gatsby-config.js
@@ -42,7 +42,8 @@ module.exports = {
         display: 'minimal-ui',
         icon: 'src/images/gatsby-icon.png', // This path is relative to the root of the site.
       },
-    },
+	},
+	'gatsby-plugin-styled-components'
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.app/offline
     // 'gatsby-plugin-offline',
