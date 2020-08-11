23cb5678a976d6519c66ad9a6e17d70a3a9e64a5
Shashank Shekhar <shashank7200@gmail.com>
2020-08-12 03:58:05 +0530

    sets default theme to black and white

--- src/store/theme/initialState.js
+++ src/store/theme/initialState.js
@@ -1,9 +1,9 @@
-import { customThemes, randomNumber } from './../../helper/constants';
-const themesArray = Object.keys(customThemes);
-const randomTheme = customThemes[themesArray[randomNumber(0, themesArray.length - 1)]];
+import { customThemes } from './../../helper/constants';
+// const themesArray = Object.keys(customThemes);
+// const randomTheme = customThemes['White & Black'];
 
 const initialState = {
-	...randomTheme
+	...customThemes['White & Black']
 }
 
 export { initialState }
