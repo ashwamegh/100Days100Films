7a4e7e9d40e80f11e35c9bc3afb7bbe033c87259
Shashank Shekhar <shashank7200@gmail.com>
2020-08-20 04:09:54 +0530

    added streaming providers logo

--- src/helper/constants.js
+++ src/helper/constants.js
@@ -25,6 +25,32 @@ export const customThemes = {
 	}
 }
 
+export const streamingProviders = {
+	"netflix": "https://res.cloudinary.com/ashwamegh/image/upload/v1597876157/StreamingProviders/netflix_y2uaj8.png",
+	"amazonprimevideo": "https://res.cloudinary.com/ashwamegh/image/upload/v1597876156/StreamingProviders/amazonprimevideo_bcmcnh.png",
+	"hotstar": "https://res.cloudinary.com/ashwamegh/image/upload/v1597876157/StreamingProviders/hotstar_iufttd.png",
+	"voot": "https://res.cloudinary.com/ashwamegh/image/upload/v1597876158/StreamingProviders/voot_vop726.png",
+	"viu": "https://res.cloudinary.com/ashwamegh/image/upload/v1597876158/StreamingProviders/viu_jonotn.png",
+	"jiocinema": "https://res.cloudinary.com/ashwamegh/image/upload/v1597876158/StreamingProviders/jiocinema_bwmppy.png",
+	"zee5": "https://res.cloudinary.com/ashwamegh/image/upload/v1597876159/StreamingProviders/zee5_pajzwa.png",
+	"itunes": "https://res.cloudinary.com/ashwamegh/image/upload/v1597876157/StreamingProviders/itunes_eero4y.png",
+	"erosnow": "https://res.cloudinary.com/ashwamegh/image/upload/v1597876156/StreamingProviders/erosnow_mrkbni.png",
+	"play": "https://res.cloudinary.com/ashwamegh/image/upload/v1597876158/StreamingProviders/playmovies_dle3ot.jpg",
+	"mubi": "https://res.cloudinary.com/ashwamegh/image/upload/v1597876157/StreamingProviders/mubi_dpsjug.png",
+	"appletvplus": "https://res.cloudinary.com/ashwamegh/image/upload/v1597876156/StreamingProviders/appletvplus_zpbb8t.png",
+	"sonyliv": "https://res.cloudinary.com/ashwamegh/image/upload/v1597876158/StreamingProviders/sonyliv_tz6o9c.jpg",
+	"youtube": "https://res.cloudinary.com/ashwamegh/image/upload/v1597876159/StreamingProviders/youtube_khzlls.png",
+	"guidedoc": "https://res.cloudinary.com/ashwamegh/image/upload/v1597876157/StreamingProviders/guidedoc_npsuws.png",
+	"netflixkids": "https://res.cloudinary.com/ashwamegh/image/upload/v1597876157/StreamingProviders/netflixkids_i0r66m.png",
+	"tubitv": "https://res.cloudinary.com/ashwamegh/image/upload/v1597876159/StreamingProviders/tubi_twhq9v.png",
+	"bookmyshow": "https://res.cloudinary.com/ashwamegh/image/upload/v1597876156/StreamingProviders/bookmyshow_lswkjh.png",
+	"yupptv": "https://res.cloudinary.com/ashwamegh/image/upload/v1597876159/StreamingProviders/yupptv_f8fgf5.png",
+	"sunnxt": "https://res.cloudinary.com/ashwamegh/image/upload/v1597876158/StreamingProviders/sunnxt_kbovlx.png",
+	"crunchyroll": "https://res.cloudinary.com/ashwamegh/image/upload/v1597876156/StreamingProviders/crunchyroll_le71uk.png",
+	"hoichoi": "https://res.cloudinary.com/ashwamegh/image/upload/v1597876157/StreamingProviders/hoichoi_tjse9f.png",
+	"altbalaji": "https://res.cloudinary.com/ashwamegh/image/upload/v1597876156/StreamingProviders/altbalaji_nyiebr.png"
+}
+
 export function randomNumber(min, max) {  
 	return Math.floor(
 		Math.random() * (max - min + 1) + min
