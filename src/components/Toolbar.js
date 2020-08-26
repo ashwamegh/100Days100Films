7c820e47109568a8f432b19b343f2861869bd3db
Shashank Shekhar <shashank7200@gmail.com>
2020-08-27 02:44:00 +0530

    added toolbar code for changing order and sorting films

--- src/components/Toolbar.js
+++ src/components/Toolbar.js
@@ -1,11 +1,12 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
+import PropTypes from 'prop-types';
 import styled from 'styled-components';
 import { ThemeContext } from './../store';
 import FilterSettings from './FilterSettings';
 import { RadioInputWrapper, RadioInput, ToolbarWrapper } from './styled';
 
 
-function Toolbar() {
+function Toolbar({ onChangeOfOrder, onSorting, sortingOrder }) {
 	const { state } = useContext(ThemeContext);
 	const RadioInputExtended = styled(RadioInput)`
 		&:checked:after {
@@ -19,23 +20,56 @@ function Toolbar() {
 	return (
 		<ToolbarWrapper>
 			<RadioInputWrapper>
-				<RadioInputExtended name="sortingOrder" id="sortByAsc" value="sortByAsc"/>
+				<RadioInputExtended
+					name="sortingOrder"
+					id="sortByAsc"
+					value="sortByAsc"
+					checked={sortingOrder === "asc"}
+					onChange={(e) => {
+						if(e.target.checked) {
+							onChangeOfOrder("asc")
+						}
+					}}
+				/>
 				<label className="form-check-label" htmlFor="sortingOrder">
 					Order By Asc
 				</label>
 			</RadioInputWrapper>
 
 			<RadioInputWrapper style={{ marginRight: 16 }}>
-				<RadioInputExtended name="sortingOrder" id="sortByDesc" value="sortByDesc" />
+				<RadioInputExtended 
+					name="sortingOrder"
+					id="sortByDesc"
+					value="sortByDesc"
+					checked={sortingOrder === "desc"}
+					onChange={(e) => {
+						if(e.target.checked) {
+							onChangeOfOrder("desc")
+						}
+					}}
+				/>
 				<label className="form-check-label" htmlFor="sortingOrder">
 					Order By Desc
 				</label>
 			</RadioInputWrapper>
 			<FilterSettings
-				onFilterSelect={() => console.log("sjndskjn")}
+				onFilterSelect={(sel) => onSorting(sel)}
 			/>
 		</ToolbarWrapper>
 	);
 }
+
+Toolbar.propTypes = {
+	onChangeOfOrder: PropTypes.func,
+	onSorting: PropTypes.func,
+	sortingOrder: PropTypes.string.isRequired
+}
+
+Toolbar.defaultProps = {
+	onChangeOfOrder: () => {},
+	onSorting:() => {}
+}
+
+
 
 export default Toolbar;
